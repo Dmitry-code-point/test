75ec2a22a428f78adf4b63bc5bdf05261cecdb0b
Dmitry <sportsboy137@gmail.com>
2025-02-22 11:39:33 +0300

    Mod3-2-2_new

--- Mod3-2-2.py
+++ Mod3-2-2.py
@@ -1,10 +1,7 @@
-import random
-n = 5
-#l = []
-m = [[random.randint(0,100)	
+from random import randint
+n = 5 
+m = [randint(0,100)
 	for i in range(n)
-	 ] 
-	 	for j in range(n)
-	]
-
-print(m)
+		for j in range(n)]
+print('Ввод данных:',m)
+print('Вывод максимального числа:',max(m))
