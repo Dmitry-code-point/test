f0297694113520c1b3c4ac93f4b721960e8d7625
Dmitry <sportsboy137@gmail.com>
2025-02-22 21:03:33 +0300

    Commit Mod3-2-2.1

--- Mod3-2-2.py
+++ Mod3-2-2.py
@@ -1,7 +1,10 @@
 from random import randint
-n = 5 
-m = [randint(0,100)
-	for i in range(n)
-		for j in range(n)]
+n = 5
+m = [[randint(0,100)
+	for i in range(n)]
+		for j in range(n)
+	]
+find_element = max(max(sublist)
+	for sublist in m)
 print('Ввод данных:',m)
-print('Вывод максимального числа:',max(m))
+print('Вывод:',find_element)
