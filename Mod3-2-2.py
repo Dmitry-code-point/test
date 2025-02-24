c5c1cefb526b0516cfbfe7e9e425f58f51d354f9
Dmitry <sportsboy137@gmail.com>
2025-02-24 19:02:30 +0300

    Commit Mod3-2-2.1_new

--- Mod3-2-2.py
+++ Mod3-2-2.py
@@ -1,9 +1,15 @@
 from random import randint
 n = 5
-m = [[randint(0,100)
-	for i in range(n)]
-		for j in range(n)
-	]
+#m = [[randint(0,100)
+#	for i in range(n)]
+#		for j in range(n)
+#	]
+m = [[9, 70, 20, 67, 33], 
+	 [60, 20, 94, 14, 77], 
+	 [27, 58, 45, 0, 13],
+	 [39, 47, 25, 97, 69],
+	 [83, 13, 100, 1, 85]
+	 ]
 find_element = max(max(sublist)
 	for sublist in m
 	)
