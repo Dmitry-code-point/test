07ba26f7763a3ea824562531d2dc79f342074437
Dmitry <sportsboy137@gmail.com>
2025-02-25 20:44:20 +0300

    Commit Mod3-3-1

---
+++ Mod3-3-1.py
@@ -0,0 +1,9 @@
+import math
+def area(a, b, c):
+	pass
+a = int(input('Первое число:'))
+b = int(input('Второе число:'))
+c = int(input('Третье число:'))
+p = (a + b + c) / 2
+s = math.sqrt(p * (p - a) * (p - b) * (p - c)) / 10
+print("Площадь треугольника:", s)
