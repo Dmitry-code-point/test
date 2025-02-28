baefc1086cc44a83b541e258d140a613d1c340f2
Dmitry <sportsboy137@gmail.com>
2025-03-01 01:39:40 +0300

    Commit Mod3-3-3_v1

--- Mod3-3-3.py
+++ Mod3-3-3.py
@@ -1,3 +1,11 @@
-a = [56,9,11,2]
-b = sorted(a,reverse=True)
-print(b)
+def sravnenie(a):
+    b = []
+    for i in a:
+        if i > 10:
+            b.append(float(i) / 10)
+        else:
+            b.append(i)
+    return b
+a = [56, 9, 11, 2]
+n = (sorted(sravnenie(a), reverse=True))
+print(n)
