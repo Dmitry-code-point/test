fd56a9d563be4a748ce3274bcc9478c5caa9c7b5
Dmitry <sportsboy137@gmail.com>
2025-03-03 22:01:24 +0300

    commit Mod4-1

---
+++ Mod4-1.py
@@ -0,0 +1,33 @@
+from heapq import merge
+from statistics import median
+
+numbers = [3, 5, 2, 7, 6, 9, 1]
+print(numbers)
+def merge_sort(numbers):
+    if len(numbers) < 2:
+        return numbers[:]
+    else:
+        median = int(len(numbers) / 2)
+        left = merge_sort(numbers[:median])
+        right = merge_sort(numbers[:median])
+        return merge(left, right)
+
+def merge(left, right):
+    res = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            res.append(left[i])
+            i += 1
+        else:
+            res.append(right[j])
+            j += 1
+    while i < len(left):
+        res.append(left[i])
+        i += 1
+    while j < len(right):
+        res.append(right[j])
+        j += 1
+    return res
+print(merge_sort(numbers))
+
