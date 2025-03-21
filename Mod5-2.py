fa7ea605076603e5569f0ebe6d1160d2e83f46fe
DESKTOP-R24FKTP\Home <opppp>
2025-03-21 23:50:37 +0300

    fix Mod5-2

--- Mod5-2.py
+++ Mod5-2.py
@@ -3,11 +3,9 @@ import math
 
 class Point:
 
-    def coordinate_old_x(self, old_x):
-        self.old_x = float(old_x)
-
-    def coordinate_old_y(self, old_y):
-        self.old_y = float(old_y)
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
 
     def vector(self, vector):
         self.vector = float(vector)
@@ -15,22 +13,23 @@ class Point:
     def tilt(self, tilt):
         self.tilt = float(tilt)
 
-    def get(self):
-        print(f"Координаты начальной точки: {self.old_x}:{self.old_y}")
+
+    def distance(self, other_point):
+        return math.sqrt(abs(self.x  - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
 
     def change_vector_x(self):
-        return (self.old_x + self.vector)* math.cos(self.tilt)
+        return (self.x + self.vector) * math.cos(self.tilt)
 
     def change_vector_y(self):
-        return (self.old_y + self.vector)* math.sin(self.tilt)
+        return (self.y + self.vector) * math.sin(self.tilt)
 
 
-while True:
-    a = Point()
-    a.coordinate_old_x(input('Введите координату Х первой точки: '))
-    a.coordinate_old_y(input('Введите координату Y первой точки: '))
-    a.vector(input('Введите длину переноса точки: '))
-    a.tilt(input('Введите градус переноса точки: '))
-    new_x = a.change_vector_x()
-    new_y = a.change_vector_y()
-    print(f"Новые координаты перенесенной точки: [{new_x}:{new_y}]")
+p1 = Point(5,3)
+p2 = Point(2,3)
+p1.vector(input('Введите длину переноса точки: '))
+p1.tilt(input('Введите градус переноса точки: '))
+distance_points = p1.distance(p2)
+print(f"Расстояние между точками: {distance_points}")
+new_x = p1.change_vector_x()
+new_y = p1.change_vector_y()
+print(f"Новые координаты перенесенной точки: [{new_x}:{new_y}]")
