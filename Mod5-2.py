62f08844d1064e06b84038fdf5adea918bfe280c
Dmitry <sportsboy137@gmail.com>
2025-03-23 13:55:17 +0300

    fix Mod5-2

--- Mod5-2.py
+++ Mod5-2.py
@@ -3,33 +3,29 @@ import math
 
 class Point:
 
-    def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+    def __init__(self, list_x, list_y):
+        self.x = float(list_x)
+        self.y = float(list_y)
 
-    def vector(self, vector):
-        self.vector = float(vector)
+    def select_vector(self, list_vector):
+        self.vector = float(list_vector)
 
-    def tilt(self, tilt):
-        self.tilt = float(tilt)
+    def select_tilt(self, list_tilt):
+        self.tilt = float(list_tilt)
 
 
     def distance(self, other_point):
         return math.sqrt(abs(self.x  - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
 
-    def change_vector_x(self):
-        return (self.x + self.vector) * math.cos(self.tilt)
-
-    def change_vector_y(self):
-        return (self.y + self.vector) * math.sin(self.tilt)
-
+    def change_vector(self):
+        return ((self.x + self.vector) * math.cos(self.tilt),
+                (self.y + self.vector) * math.sin(self.tilt))
 
 p1 = Point(5,3)
 p2 = Point(2,3)
-p1.vector(input('Введите длину переноса точки: '))
-p1.tilt(input('Введите градус переноса точки: '))
+p1.select_vector(input('Введите длину переноса точки: '))
+p1.select_tilt(input('Введите градус переноса точки: '))
 distance_points = p1.distance(p2)
 print(f"Расстояние между точками: {distance_points}")
-new_x = p1.change_vector_x()
-new_y = p1.change_vector_y()
-print(f"Новые координаты перенесенной точки: [{new_x}:{new_y}]")
+change_point = p1.change_vector()
+print(f"Новые координаты перенесенной точки: {change_point}")
