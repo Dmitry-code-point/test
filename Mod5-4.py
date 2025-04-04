20beede5737eddb1fe20304b70a96cf7d89f1d3e
Dmitry <sportsboy137@gmail.com>
2025-04-04 22:07:32 +0300

    fix Mod5-4

--- Mod5-4.py
+++ Mod5-4.py
@@ -2,19 +2,29 @@ import json
 
 
 class Model:
-    def Save_to_file(self, words):
+    def save_to_file(self, words):
+        attributes = dir(words)
+        word_keys = list(words.keys())
+        combined_attribute = attributes + word_keys
         with open('words.json', 'w') as file:
-            json.dump(words, file, ensure_ascii=False)
+            json.dump(combined_attribute, file, ensure_ascii=False)
 
-    def Open_file(self,):
+    def open_file(self,):
         with open('words.json', 'r') as file:
             data_words = json.load(file)
-            print(data_words)
+            data_words1 = list(filter(lambda item: not item.startswith('_'), data_words)) # Избавление  от служеюных аттрибут
+            data_words3 = []
+            for item in data_words1:
+                if item in words:
+                    data_words3.append(item)
+            print(data_words) # Вывод задания IN 2 - OUT 2
+            print (data_words3)
 
 
 model = Model()
 
-words = {"title": "1", "text": 2, "author": "3"}
+words = {'title': 1, 'text': 2, 'author': 3}
 
-model.Save_to_file(words)
-model.Open_file()
+model.save_to_file(words)
+model.open_file()
+
