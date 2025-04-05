34a42499a5e6cd8d34c27f3710eb5212999e778e
Dmitry <sportsboy137@gmail.com>
2025-04-05 11:32:12 +0300

    refactoring Mod5-4

--- Mod5-4.py
+++ Mod5-4.py
@@ -1,6 +1,5 @@
 import json
 
-
 class Model:
     def save_to_file(self, words):
         attributes = dir(words)
@@ -12,22 +11,19 @@ class Model:
     def open_file(self):
         with open('words.json', 'r') as file:
             data_words = json.load(file)
-            print(data_words)  # Вывод задания IN 2 - OUT 2
+            print(data_words) # Вывод задания IN 2 - OUT 2
             return self.get_attribute(data_words)
 
     def get_attribute(self, data):
-        data_words1 = list(filter(lambda item: not item.startswith('_'), data))  # Избавление  от служеюных аттрибут
+        data_words1 = list(filter(lambda item: not item.startswith('_'), data)) # Избавление  от служеюных аттрибут
         data_words2 = []
         for item in data_words1:
             if item in words:
                 data_words2.append(item)
         print(data_words2)
 
-
 model = Model()
-
 words = {'title': 1, 'text': 2, 'author': 3}
-
 model.save_to_file(words)
 model.open_file()
 
