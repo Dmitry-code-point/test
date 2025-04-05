54d60c6bea779e455eafd21f4eb7237a12a70639
Dmitry <sportsboy137@gmail.com>
2025-04-05 11:11:50 +0300

    refactoring Mod5-4

--- Mod5-4.py
+++ Mod5-4.py
@@ -9,16 +9,19 @@ class Model:
         with open('words.json', 'w') as file:
             json.dump(combined_attribute, file, ensure_ascii=False)
 
-    def open_file(self,):
+    def open_file(self):
         with open('words.json', 'r') as file:
             data_words = json.load(file)
-            data_words1 = list(filter(lambda item: not item.startswith('_'), data_words)) # Избавление  от служеюных аттрибут
-            data_words2 = []
-            for item in data_words1:
-                if item in words:
-                    data_words2.append(item)
-            print(data_words) # Вывод задания IN 2 - OUT 2
-            print (data_words2)
+            return self.get_attribute(data_words)
+
+    def get_attribute(self, data_words):
+        data_words1 = list(filter(lambda item: not item.startswith('_'), data_words))  # Избавление  от служеюных аттрибут
+        data_words2 = []
+        for item in data_words1:
+            if item in words:
+                data_words2.append(item)
+        print(data_words)  # Вывод задания IN 2 - OUT 2
+        print(data_words2)
 
 
 model = Model()
