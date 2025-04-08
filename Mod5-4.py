8ee49db00222ce196dbb4c89f18163136aded375
Dmitry-code-point <sportsboy137@gmail.com>
2025-04-08 12:59:10 +0300

    Update Mod5-4.py

--- Mod5-4.py
+++ Mod5-4.py
@@ -1,27 +1,32 @@
 import json
 
+class Model:
 
-def save_to_file(words):
-    attributes = dir(words) + list(words.keys())
-    with open('words.json', 'w') as file:
-        json.dump(attributes, file, ensure_ascii=False)
+    def __init__(self, word1):
+        self.word1 = word1
 
-def open_file():
-    with open('words.json', 'r') as file:
-        data_attributes = json.load(file)
-        print(data_attributes) # Вывод задания IN 2 - OUT 2
-        return get_attribute(data_attributes)
+    def save_to_file(self):
+        attributes = dir(self.word1) + list(self.word1.keys())
+        with open('words.json', 'w') as file:
+            json.dump(attributes, file, ensure_ascii=False)
 
-def get_attribute(data_words):
-    data_words1 = list(filter(lambda item: not item.startswith('_'), data_words)) # Избавление  от служеюных аттрибут
-    data_words2 = []
-    for item in data_words1:
-        if item in words:
-            data_words2.append(item)
-    print(data_words2)
+    def open_file(self):
+        with open('words.json', 'r') as file:
+            data_attributes = json.load(file)
+            print(data_attributes) # Вывод задания IN 2 - OUT 2
+            return self.get_attribute(data_attributes)
+
+    def get_attribute(self, data_words):
+        data_words1 = list(filter(lambda item: not item.startswith('_'), data_words)) # Избавление  от служеюных аттрибут
+        data_words2 = []
+        for item in data_words1:
+            if item in words:
+                data_words2.append(item)
+        print(data_words2)
 
 
 words = {'title': 1, 'text': 2, 'author': 3}
-save_to_file(words)
-words = open_file()
+m = Model(words)
+m.save_to_file()
+m.open_file()
 
