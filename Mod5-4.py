78ce825caa93cee6208a80fdf3bab03169a12aad
Dmitry-code-point <sportsboy137@gmail.com>
2025-04-09 13:28:08 +0300

    Update Mod5-4.py

--- Mod5-4.py
+++ Mod5-4.py
@@ -1,32 +1,33 @@
 import json
 
 class Model:
-
-    def __init__(self, word1):
-        self.word1 = word1
+    def __init__(self, title, text, author):
+        self.title = title
+        self.text = text
+        self.author = author
 
     def save_to_file(self):
-        attributes = dir(self.word1) + list(self.word1.keys())
+        all_attributes = dir(self)
         with open('words.json', 'w') as file:
-            json.dump(attributes, file, ensure_ascii=False)
+            json.dump(all_attributes, file, ensure_ascii=False)
 
     def open_file(self):
         with open('words.json', 'r') as file:
             data_attributes = json.load(file)
-            print(data_attributes) # Вывод задания IN 2 - OUT 2
+            print(data_attributes)  # Вывод задания IN 2 - OUT 2
             return self.get_attribute(data_attributes)
 
     def get_attribute(self, data_words):
-        data_words1 = list(filter(lambda item: not item.startswith('_'), data_words)) # Избавление  от служеюных аттрибут
-        data_words2 = []
+        data_words1 = list(
+            filter(lambda item: not item.startswith('_'), data_words))  # Избавление  от служеюных аттрибут
+        name_functions = ['save_to_file', 'open_file', 'get_attribute']
+        attributes = []
         for item in data_words1:
-            if item in words:
-                data_words2.append(item)
-        print(data_words2)
+            if item not in name_functions:
+                attributes.append(item)
+        print(attributes)
 
-
-words = {'title': 1, 'text': 2, 'author': 3}
-m = Model(words)
+m = Model(1, 2, 3)
 m.save_to_file()
 m.open_file()
 
