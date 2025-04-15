29974a7680ad1b4508b84f0bee01f8d8abdd3568
Dmitry-code-point <sportsboy137@gmail.com>
2025-04-15 15:12:39 +0300

    Update Mod6-1.py

--- Mod6-1.py
+++ Mod6-1.py
@@ -2,13 +2,12 @@ import time
 from threading import Thread
 
 
-def get_thread(thread_name):
+def get_thread(item):
     time.sleep(1)
-    return thread_name
+    return print(item)
+
 
 names = ['Name1', 'Name2', 'Name3', 'Name4', 'Name5']
 
 for item in names:
-    thread_names = get_thread(item)
-    print(thread_names)
-
+    get_thread(item)
