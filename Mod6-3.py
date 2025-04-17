a7f5513adb823968c61e33d8bdecedbd5e3f995a
Dmitry <sportsboy137@gmail.com>
2025-04-17 21:01:09 +0300

    update Mod6-3

--- Mod6-3.py
+++ Mod6-3.py
@@ -7,10 +7,20 @@ import requests
 
 def get_html(item):
     response = requests.get(item)
-    #print(response.text)
-    print(response.status_code)
+    return print(response.text)
 
-html_name = ['https://yandex.ru', 'https://google.com', 'https://ru.wikipedia.org', 'https://dzen.ru/?yredirect=true']
+html_name = ['https://yandex.ru', 'https://google.com', 'https://ru.wikipedia.org', 'https://dzen.ru/?yredirect=true', 'https://mail.ru/']
+t1 = datetime.now()
 
 for name in html_name:
     get_html(name)
+
+#threads = [Thread(target=get_html, args=(item, )) for item in html_name]
+
+#for t in threads:
+#    t.start()
+
+#for t in threads:
+#    t.join()
+
+print('Затраченное время:', (datetime.now()- t1).seconds)
